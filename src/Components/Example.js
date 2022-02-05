ffdbe133122f1578d0308dcb9d595c8896c2716e
murad-narimanli <murad.narimanli@mail.ru>
2022-02-05 23:46:07 +0400

    custom toolto[ added

--- src/Components/Example.js
+++ src/Components/Example.js
@@ -13,6 +13,25 @@ export default class extends React.Component {
         highlightLinkIndexes: []
     }
 
+    showToolTip(evt, link){
+        let tooltip = document.getElementById("tooltip");
+        tooltip.innerHTML = `
+           <div>
+            <p>  ${link.source.name} -> ${link.target.name} </p>
+            <p> Weight ${link.width.toFixed(2)} </p>
+          </div>
+        `
+        tooltip.style.visibility = 'visible';
+        tooltip.style.left = evt.pageX - 140 + 'px';
+        tooltip.style.top = evt.pageY - 150 + 'px';
+    }
+
+
+     hideTooltip() {
+        let tooltip = document.getElementById("tooltip");
+        tooltip.style.visibility = "hidden";
+    }
+
     render() {
         const {
             data,
@@ -29,79 +48,87 @@ export default class extends React.Component {
         if (width < 10) return null;
 
         return (
-            <svg width={width + margin.left + margin.right} height={height + margin.top + margin.bottom}>
-                <Sankey
-                    top={margin.top}
-                    left={margin.left}
-                    data={data}
-                    size={[width, height]}
-                    nodeWidth={15}
-                    nodePadding={10}
-                    extent={[[1, 1], [width - 1, height - 6]]}
+            <div>
+                <div id="tooltip"  style={{position: 'absolute', visibility: 'hidden'}}/>
 
-                >
-                    {({ data }) => (
-                        <Group>
-                            {data.nodes.map((node, i) => (
-                                <Group top={node.y0} left={node.x0} key={`node-${i}`}>
-                                    <rect
-                                        id={`rect-${i}`}
-                                        width={node.x1 - node.x0}
-                                        height={node.y1 - node.y0}
-                                        fill={node.color}
-                                        opacity={0.9}
-                                        stroke="white"
-                                        strokeWidth={2}
-                                        onMouseOver={e => {
-                                            this.setState({
-                                                highlightLinkIndexes: [...node.sourceLinks.map(l => l.index), ...node.targetLinks.map(l => l.index) ] })
-                                        }}
-                                        onMouseOut={e => {
-                                            this.setState({ highlightLinkIndexes: [] })
-                                        }}
-                                    />
-                                    <Text
-                                        x={18}
-                                        y={((node.y1 - node.y0) / 2)}
-                                        verticalAnchor="middle"
-                                        style={{
-                                            font: '10px sans-serif'
-                                        }}
-                                    >
-                                        {node.name}
-                                    </Text>
+                <svg width={width + margin.left + margin.right} height={height + margin.top + margin.bottom}>
+                    <Sankey
+                        top={margin.top}
+                        left={margin.left}
+                        data={data}
+                        size={[width, height]}
+                        nodeWidth={15}
+                        nodePadding={10}
+                        extent={[[1, 1], [width - 1, height - 6]]}
 
-                                </Group>
-                            ))}
-
+                    >
+                        {({ data }) => (
                             <Group>
-                                {data.links.map((link, i) => (
-                                    <>
-                                        <path
-                                            style={{cursor:'pointer' , transition:'0.2s ease-in-out'}}
-                                            key={`link-${i}`}
-                                            d={path(link)}
-                                            // stroke='black'
-                                            stroke={this.state.highlightLinkIndexes.includes(i) ? 'red' : 'black'}
-                                            strokeWidth={Math.max(1, link.width)}
-                                            // opacity={0.2}
-                                            opacity={this.state.highlightLinkIndexes.includes(i) ? 0.7 : 0.15}
-                                            fill="none"
+                                {data.nodes.map((node, i) => (
+                                    <Group top={node.y0} left={node.x0} key={`node-${i}`}>
+                                        <rect
+                                            id={`rect-${i}`}
+                                            width={node.x1 - node.x0}
+                                            height={node.y1 - node.y0}
+                                            fill={node.color}
+                                            opacity={0.9}
+                                            stroke="white"
+                                            strokeWidth={2}
                                             onMouseOver={e => {
-                                                this.setState({ highlightLinkIndexes: [i] })
+                                                this.setState({
+                                                    highlightLinkIndexes: [...node.sourceLinks.map(l => l.index), ...node.targetLinks.map(l => l.index) ] })
                                             }}
                                             onMouseOut={e => {
                                                 this.setState({ highlightLinkIndexes: [] })
                                             }}
                                         />
-                                    </>
-                                ))}
-                            </Group>
+                                        <Text
+                                            x={18}
+                                            y={((node.y1 - node.y0) / 2)}
+                                            verticalAnchor="middle"
+                                            style={{
+                                                font: '10px sans-serif'
+                                            }}
+                                        >
+                                            {node.name}
+                                        </Text>
 
-                        </Group>
-                    )}
-                </Sankey>
-            </svg>
+                                    </Group>
+                                ))}
+
+                                <Group>
+                                    {data.links.map((link, i) => (
+                                        <>
+                                            <path
+                                                style={{cursor:'pointer' , transition:'0.2s ease-in-out'}}
+                                                key={`link-${i}`}
+                                                d={path(link)}
+                                                // stroke='black'
+                                                stroke={this.state.highlightLinkIndexes.includes(i) ? 'red' : 'black'}
+                                                strokeWidth={Math.max(1, link.width)}
+                                                // opacity={0.2}
+                                                opacity={this.state.highlightLinkIndexes.includes(i) ? 0.7 : 0.15}
+                                                fill="none"
+                                                onMouseOver={e => {
+                                                    this.showToolTip(e , link)
+                                                    this.setState({ highlightLinkIndexes: [i] })
+                                                }}
+                                                onMouseOut={e => {
+                                                    this.hideTooltip()
+                                                    this.setState({ highlightLinkIndexes: [] })
+                                                }}
+                                            />
+
+
+                                        </>
+                                    ))}
+                                </Group>
+
+                            </Group>
+                        )}
+                    </Sankey>
+                </svg>
+            </div>
         );
     }
 }
